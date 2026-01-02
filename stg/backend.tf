b572f746dc3e1987285fbf5e5516d960499a71bf
toshiki003 <zou3road@gmail.com>
2026-01-02 23:21:53 +0900

    add: 初期接続成功

---
+++ stg/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket       = "cp-terraform-satoshi-sekime-stg"
+    key          = "main.tfstate"
+    region       = "ap-northeast-1"
+  }
+}
---
+++ stg/variables.tf
@@ -0,0 +1,5 @@
+locals {
+  env                  = "stg"
+  account_id           = "488639172203"
+  region               = "ap-northeast-1"
+}
